536b8007a0fe6f1bb22418501e5392d686f7c87a
Santiago Vargas Vega <santiagovargasve@gmail.com>
2021-04-21 15:21:58 -0500

    Handling exceptions for calculating Ngrams done

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 
 def calculateNGrams(text,n):
-    pass
-
+    if len(text)<n:
+        raise Exception("Integer larger than text, not available N-gram for this")
+    else:
+        for index in range(0, len(text)):
+            pass
+    
 def mostFrequentNgram(text ,  n ):
     pass
 
+
+
+if __name__ == '__main__':
+    calculateNGrams('Slang',2)
