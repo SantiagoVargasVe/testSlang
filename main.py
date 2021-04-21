f949877cd315cfabf5ae16c28c23bbfa659c3006
Santiago Vargas Vega <santiagovargasve@gmail.com>
2021-04-21 15:34:52 -0500

    Ngrams method completed.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 
 def calculateNGrams(text,n):
+    exact_divide = True if len(text)%n==0 else False
+    n_grams= []
     if len(text)<n:
         raise Exception("Integer larger than text, not available N-gram for this")
     else:
-        for index in range(0, len(text)):
-            pass
-    
-def mostFrequentNgram(text ,  n ):
+        index = 0
+        while n+index<= len(text):
+            if n+index<= len(text):
+                n_grams.append(text[index:n+index])
+            index +=1
+    print(n_grams)
+def mostFrequentNgram(text ,n):
     pass
 
 
 
 if __name__ == '__main__':
-    calculateNGrams('Slang',2)
+    calculateNGrams('Slang',5)
